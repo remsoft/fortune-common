6688474a3fe15c0dad9fb1cc564936a2b4946864
remsoft <rem.soft.corp@gmail.com>
2018-09-09 19:22:09 +0800

    Module Supplier Customer
    Module Supplier Customer

---
+++ src/main/java/com/rem/fortune/model/Address.java
@@ -0,0 +1,63 @@
+package com.rem.fortune.model;
+
+public class Address {
+	private String id;
+	private String street;
+	private String city;
+	private String state;
+	private String zip;
+	private String country;
+	private String attention;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getAttention() {
+		return attention;
+	}
+	public void setAttention(String attention) {
+		this.attention = attention;
+	}
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", street=" + street + ", city=" + city + ", state=" + state + ", zip=" + zip
+				+ ", country=" + country + ", attention=" + attention + ", getId()=" + getId() + ", getStreet()="
+				+ getStreet() + ", getCity()=" + getCity() + ", getState()=" + getState() + ", getZip()=" + getZip()
+				+ ", getCountry()=" + getCountry() + ", getAttention()=" + getAttention() + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+	}
+	
+	
+}
--- src/main/java/com/rem/fortune/model/CustomerSupplier.java
+++ src/main/java/com/rem/fortune/model/CustomerSupplier.java
@@ -1,9 +1,12 @@
 package com.rem.fortune.model;
 
 public class CustomerSupplier extends Model {
+	private String id;
 	private String name;
+	private String phone;
 	private String email;
-	private String isCustomer;
+	private Address address;
+	private int isCustomer;
 	public String getName() {
 		return name;
 	}
@@ -15,14 +18,35 @@ public class CustomerSupplier extends Model {
 	}
 	public void setEmail(String email) {
 		this.email = email;
-	}
-	public String getIsCustomer() {
+	}	
+	public int getIsCustomer() {
 		return isCustomer;
 	}
-	public void setIsCustomer(String isCustomer) {
+	public void setIsCustomer(int isCustomer) {
 		this.isCustomer = isCustomer;
 	}
-
-	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "CustomerSupplier [id=" + id + ", name=" + name + ", phone=" + phone + ", email=" + email + ", address="
+				+ address.toString() + ", isCustomer=" + isCustomer + "]";
+	}
 	
 }
